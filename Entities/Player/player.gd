a43de2ab39e6c464e9e15b9c9c177f9fa43b9977
David-Parkerr <davidparker0040@gmail.com>
2025-05-17 13:42:47 -0400

    New Input handling

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var movement_direction = Vector2.ZERO
 var input_handler = InputHandler.new()
 
 var screen_size
@@ -12,8 +13,30 @@ func _physics_process(delta):
 	move_handler(delta)
 	handle_animations()
 
+func _input(event):
+	if event.is_action("move_left"):
+		if event.is_pressed() and not event.is_echo():
+			movement_direction.x += -1
+		elif not event.is_pressed():
+			movement_direction.x += 1
+	if event.is_action("move_right"):
+		if event.is_pressed() and not event.is_echo():
+			movement_direction.x += 1
+		elif not event.is_pressed():
+			movement_direction.x += -1
+	if event.is_action("move_up"):
+		if event.is_pressed() and not event.is_echo():
+			movement_direction.y += -1
+		elif not event.is_pressed():
+			movement_direction.y += 1
+	if event.is_action("move_down"):
+		if event.is_pressed() and not event.is_echo():
+			movement_direction.y += 1
+		elif not event.is_pressed():
+			movement_direction.x += -1
+
 func move_handler(delta):
-	velocity = input_handler.get_normalized_input_direction() * speed
+	velocity = movement_direction * speed
 	position += velocity * delta
 
 func handle_animations():
