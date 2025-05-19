b7c55d85c980bb6788da3af1dbb5f75a39e7c350
David-Parkerr <davidparker0040@gmail.com>
2025-05-19 02:47:44 -0400

    Better character movement physics

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-@export var speed = 10.0
-@export var jump_power = 10.0
-var speed_multiplier = 30.0
-var jump_multiplier = -30.0
-var direction = 0
-var is_character_mode = true
-var impulse_strength = 15.0
+@export var speed: float = 450.0
+@export var jump_power: float = 450.0
+@export var max_vertical_velocity: float = 3500.0
+@export var max_jump_time: float = 0.25
+var impulse_strength: float = 15.0
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var is_character_mode = true
 var pending_physics_mode = false
 var pending_character_mode = false
+var is_jumping = false
+var has_canceled_jump = false
 
 var camera_position = Vector2.ZERO
+var previous_jump_time = Time.get_unix_time_from_system()
+var time_between_jump
 
 func _ready() -> void:
 	set_body_state($RigidBody2D, false, true, 0, 0)
@@ -55,6 +59,10 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_released("left_click"):
 		pending_character_mode = true
 
+func _process(delta: float) -> void:
+	if not is_jumping:
+		previous_jump_time = Time.get_unix_time_from_system()
+
 func _physics_process(delta: float) -> void:
 	if pending_physics_mode:
 		switch_to_physics_mode()
@@ -64,20 +72,56 @@ func _physics_process(delta: float) -> void:
 		pending_character_mode = false
 	
 	if is_character_mode:
-		camera_position = $CharacterBody2D.global_position
-		direction = Input.get_axis("move_left", "move_right")
-		if direction:
-			$CharacterBody2D.velocity.x = direction * speed * speed_multiplier
-		else:           
-			$CharacterBody2D.velocity.x = move_toward($CharacterBody2D.velocity.x, 0, speed * speed_multiplier)
-		$CharacterBody2D.move_and_slide()
+		character_process_handler(delta)
 	else:
-		camera_position = $RigidBody2D.global_position
+		swinging_process_handler(delta)
 	
 	# Place camera according to current body
 	$Camera2D.global_position = $Camera2D.global_position.lerp(camera_position, 0.1)
 	handle_animations()
 	
+
+func character_process_handler(delta):
+	camera_position = $CharacterBody2D.global_position
+	
+	# Falling / jumping boolean setters
+	if $CharacterBody2D.is_on_floor() or Input.is_action_just_released("move_up"):
+		is_jumping = false
+	if Time.get_unix_time_from_system() - previous_jump_time >= max_jump_time:
+		has_canceled_jump = true
+		is_jumping = false
+	
+	horizontal_movement_handler()
+	vertical_movement_handler(delta)
+	
+	$CharacterBody2D.move_and_slide()
+
+func horizontal_movement_handler():
+	var horizontal = Input.get_axis("move_left", "move_right")
+	if horizontal:
+		$CharacterBody2D.velocity.x = horizontal * speed
+	else:           
+		$CharacterBody2D.velocity.x = move_toward($CharacterBody2D.velocity.x, 0, speed)
+
+func vertical_movement_handler(delta):
+	# First started jumping
+	if Input.is_action_just_pressed("move_up") and $CharacterBody2D.is_on_floor():
+		is_jumping = true
+		has_canceled_jump = false;
+		
+	if is_jumping:
+		$CharacterBody2D.velocity.y = -(jump_power)
+	else:
+		# Handle gravity
+		if $CharacterBody2D.velocity.y < max_vertical_velocity:
+			$CharacterBody2D.velocity.y += gravity * delta
+	if has_canceled_jump:
+		$CharacterBody2D.velocity.y += lerp(0.0, -jump_power, 0.1)
+		has_canceled_jump = false
+
+func swinging_process_handler(delta):
+	camera_position = $RigidBody2D.global_position
+
 func handle_animations():
 	if is_character_mode:
 		if $CharacterBody2D.velocity.x > 0:
