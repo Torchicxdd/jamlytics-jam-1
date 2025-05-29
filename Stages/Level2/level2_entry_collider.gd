9d39caab96142d7d0ec5aabf9e40a523da34bda1
David-Parkerr <davidparker0040@gmail.com>
2025-05-29 04:52:29 -0400

    Add dynamic level loading

--- Stages/Level2/level2_entry_collider.gd
+++ Stages/Level2/level2_entry_collider.gd
@@ -4,3 +4,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.health_constant = 11
+		Global.world.load_level(3, Vector2(1216, -3648))
--- Stages/Level4/level4_entry_collider.gd
+++ Stages/Level4/level4_entry_collider.gd
@@ -4,3 +4,5 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.health_constant = 8
+		Global.world.unload_level(1)
+		Global.world.load_level(5, Vector2(960, -5376))
