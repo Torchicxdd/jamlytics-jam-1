dc560c27e0c7608b998fa7959e2bba9e35f051a7
Kunyro <kunyro@protonmail.ch>
2025-05-29 16:52:21 -0400

    add level 6

--- Stages/Level6/level6_entry_collider.gd
+++ Stages/Level6/level6_entry_collider.gd
@@ -4,5 +4,5 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.health_constant = 8
-		Global.world.unload_level(2)
-		Global.world.load_level(6)
+		Global.world.unload_level(3)
+		Global.world.load_level(7)
