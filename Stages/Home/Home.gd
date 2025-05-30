5ed1eca73f079cbbf2e51fc887eadb7d350c0b39
David-Parkerr <davidparker0040@gmail.com>
2025-05-30 08:54:56 -0400

    Change new game to world tscn

--- Stages/Home/Home.gd
+++ Stages/Home/Home.gd
@@ -4,7 +4,7 @@ func _process(delta):
 	pass
 
 func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://Stages/Tutorial.tscn")
+	get_tree().change_scene_to_file("res://Stages/World.tscn")
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Stages/Options/Options.tscn")
