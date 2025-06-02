ed8cc7706835d49dba3aa4f1aff84d00021d7404
You.Ran.Wang <thatrandomguy666666@gmail.com>
2025-06-01 22:21:39 -0400

    Rename 8 to 7 and add to main.

--- Stages/Level8/level7_entry_collider.gd
+++ Stages/Level8/level7_entry_collider.gd
@@ -4,8 +4,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.health_constant = 8
-		Global.world.unload_level(5)
+		Global.world.unload_level(4)
+		Global.world.load_level(5)
 		Global.world.load_level(6)
 		Global.world.load_level(7)
 		Global.world.load_level(8)
-		Global.world.load_level(9)
